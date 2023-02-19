2d3090f2caf6e3aa1f643592a9fd9fc6ad01ffa2
Bruno <brunoburkos@gmail.com>
2023-02-19 16:15:33 +0100

    Añadir sprites de fantasmas y las pruebas

--- Sprites/prueba2Sprites.py
+++ Sprites/prueba2Sprites.py
@@ -7,6 +7,7 @@ yellow = (255, 255, 0)
 pygame.init()
 
 class Phantom(pygame.sprite.Sprite):
+<<<<<<< HEAD
 
     def  __init__(self, color, number, x, y):
         super().__init__()
@@ -28,6 +29,23 @@ class Phantom(pygame.sprite.Sprite):
     def animation(self):
         self.change_state(((self.number + 1) % 12) + 1)
 
+=======
+    def __init__(self1):
+        super().__init__()
+        self1.image = pygame.image.load("PinkPhant1.png").convert()
+        self1.image.set_colorkey(yellow)
+        self1.rect = self1.image.get_rect()
+    def __init__(self2):
+        super().__init__()
+        self2.image = pygame.image.load("PinkPhant2.png").convert()
+        self2.image.set_colorkey(yellow)
+        self2.rect = self2.image.get_rect()
+    def __init__(self3):
+        super().__init__()
+        self3.image = pygame.image.load("PinkPhant3.png").convert()
+        self3.image.set_colorkey(yellow)
+        self3.rect = self3.image.get_rect()
+>>>>>>> 4e0cfdb (Añadir sprites de fantasmas y las pruebas)
 
 class Jugador(pygame.sprite.Sprite):
     def __init__(self):
@@ -41,9 +59,17 @@ clock = pygame.time.Clock()
 done = False
 
 phant_list = pygame.sprite.Group()
+<<<<<<< HEAD
 all_sprite_list = pygame.sprite.Group() 
 for i in range(1):
     phant = Phantom("Pink", 1, 200, 300)
+=======
+all_sprite_list = pygame.sprite.Group()
+for i in range(2):
+    phant = Phantom()
+    phant.rect.x = random.randrange(900)
+    phant.rect.y = random.randrange(600)
+>>>>>>> 4e0cfdb (Añadir sprites de fantasmas y las pruebas)
     
     phant_list.add(phant)
     all_sprite_list.add(phant)
@@ -57,6 +83,7 @@ while not done:
             done = True
 
     mouse_pos = pygame.mouse.get_pos()
+<<<<<<< HEAD
     phant.rect.x = mouse_pos[0]
     phant.rect.y = mouse_pos[1]
     
@@ -67,5 +94,16 @@ while not done:
     
     pygame.display.flip()
     clock.tick(12)
+=======
+    jugador.rect.x = mouse_pos[0]
+    jugador.rect.y = mouse_pos[1]
+    
+    screen.fill(black)
+    
+    all_sprite_list.draw(screen)
+    
+    pygame.display.flip()
+    clock.tick(60)
+>>>>>>> 4e0cfdb (Añadir sprites de fantasmas y las pruebas)
     
 pygame.quit()
