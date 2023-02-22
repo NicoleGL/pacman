95a03d8969fd0e54260e16d95d7109d780a654b6
Nicole <nicole.glanga@gmail.com>
2023-02-22 11:06:05 +0100

    Cambiar movimiento

--- main.py
+++ main.py
@@ -25,27 +25,18 @@ def draw_sprite(spr):
     screen.blit(spr.image, spr.rect)
     pygame.display.update()
 
-def move_sprite(spr, movement):
-    spr.rect.x += movement[0]
-    spr.rect.y += movement[1]
+def move_sprite(spr):
+    spr.rect.x += spr.speed[0]
+    spr.rect.y += spr.speed[1]
     draw_sprite(spr)
 
-def stop_at_wall(spr, xvel, yvel):
-    if((spr.direction == "left" and spr.rect.x < 3) or
-      (spr.direction == "right" and spr.rect.x > (WIDTH - (spr.rect.width + 3)))):
-        return(0, yvel)
-    elif((spr.direction == "up" and spr.rect.y < 3) or
-        (spr.direction == "down" and spr.rect.y > (HEIGHT - (spr.rect.height + 3)))):
-        return(xvel, 0)
-    else:
-        return(xvel,yvel)
-
-
+def read_chunk(spr):
+    chunk_x = spr.rect.x // 32
+    chunk_y = spr.rect.y // 32
+    return (chunk_x, chunk_y)
 
 def main():
     run = True
-    xvel = 0
-    yvel = 0
     speed = 3
     while run:
         for event in pygame.event.get():
@@ -55,24 +46,21 @@ def main():
 
                 if event.key == K_UP:
                     player.direction = "up"
-                    xvel = 0
-                    yvel = -speed
+                    player.speed = (0, -speed)
                 elif event.key == K_DOWN:
                     player.direction = "down"
-                    xvel = 0
-                    yvel = speed
+                    player.speed = (0, speed)
                 elif event.key == K_LEFT:
                     player.direction = "left"
-                    xvel = -speed
-                    yvel = 0
+                    player.speed = (-speed, 0)
                 elif event.key == K_RIGHT:
                     player.direction = "right"
-                    xvel = speed
-                    yvel = 0
+                    player.speed = (speed, 0)
         
         
         player.update_img()
-        move_sprite(player, stop_at_wall(player, xvel, yvel))
+        player.stop_if_wall(WIDTH, HEIGHT)
+        move_sprite(player)
         all_sprite_list.draw(screen)
         pygame.display.update()
         clock.tick(FPS)
