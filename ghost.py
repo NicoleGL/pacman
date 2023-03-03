44e54be256f69f96e5da85f964ed670c24b41ac3
PabloPI4 <pif2110@gmail.com>
2023-03-04 00:44:43 +0100

    PORFA FUNCIONA

--- ghost.py
+++ ghost.py
@@ -19,8 +19,10 @@ class Ghost(pygame.sprite.Sprite):
         self.y = y
         self.rect.x = x
         self.rect.y = y
+        self.dire = (0,0)
+        self.pospacAnt = (9,8)
+        self.counter = 0
 
-        self.posAnt = (8,6)
         self.df = pd.DataFrame(0, index=[(2,2),(2,4),(2,6),(2,8),(2,10),(4,4),(4,6),(4,8),(6,2),(6,4),(6,6),(6,10),
         (8,8),(8,10),(10,8),(10,10),(12,2),(12,4),(12,6),(12,10),(14,4),(14,6),(14,8),(16,2),
         (16,4),(16,6),(16,8),(16,10)], columns=[(1,0), (-1,0), (0,1), (0,-1)])
@@ -30,32 +32,76 @@ class Ghost(pygame.sprite.Sprite):
 
     def moveGhost(self, box, pospac):
         if self.color == "Red":
-            print(Ghost.rojo(box, (self.rect.x//32, self.rect.y//32)))
-            self.rect.x += Ghost.rojo(box, (self.rect.x//32, self.rect.y//32), pospac)[0]
-            self.rect.y += Ghost.rojo(box, (self.rect.x//32, self.rect.y//32), pospac)[1]
-            
+            if self.rect.x%32 == 0 and self.rect.y%32 == 0:
+                velocity = Ghost.rojo(self.counter, self.dire, box, ((self.rect.x)//32, (self.rect.y)//32), pospac)
+                self.rect.x += velocity[0]
+                self.rect.y += velocity[1]
+                self.dire = velocity
+            else:
+                self.rect.x += self.dire[0]
+                self.rect.y += self.dire[1]
+            self.counter += 1
         elif self.color == "Orange":
-            self.rect.x += Ghost.naranja(box, (self.rect.x//32, self.rect.y//32), pospac)[0]
-            self.rect.y += Ghost.naranja(box, (self.rect.x//32, self.rect.y//32), pospac)[1]
+            if self.rect.x%32 == 0 and self.rect.y%32 == 0:
+                velocity = Ghost.naranja(self.counter, self.dire, box, ((self.rect.x)//32, (self.rect.y)//32), pospac)
+                self.rect.x += velocity[0]
+                self.rect.y += velocity[1]
+                self.dire = velocity
+            else:
+                self.rect.x += self.dire[0]
+                self.rect.y += self.dire[1]
+            self.counter += 1
         elif self.color == "Pink":
-            self.rect.x += Ghost.rosa(box, (self.rect.x//32, self.rect.y//32), self.df, pospac, self.esquina)[0]
-            self.rect.y += Ghost.rosa(box, (self.rect.x//32, self.rect.y//32), self.df, pospac, self.esquina)[1]
+            if self.rect.x%32 == 0 and self.rect.y%32 == 0:
+                velocity = Ghost.rosa(self.counter, self.dire, box, ((self.rect.x)//32, (self.rect.y)//32), self.df, pospac, self.esquina)
+                self.rect.x += velocity[0]
+                self.rect.y += velocity[1]
+                self.dire = velocity
+            else:
+                self.rect.x += self.dire[0]
+                self.rect.y += self.dire[1]
+            self.counter += 1
         elif self.color == "Blue":
-            self.rect.x += Ghost.azul(box, (self.rect.x//32, self.rect.y//32), self.df, pospac, self.esquina)[0]
-            self.rect.y += Ghost.azul(box, (self.rect.x//32, self.rect.y//32), self.df, pospac, self.esquina)[1]
+            if self.rect.x%32 == 0 and self.rect.y%32 == 0:
+                velocity = Ghost.azul(self.counter, self.dire, box, ((self.rect.x)//32, (self.rect.y)//32), self.df, pospac, self.esquina)
+                self.rect.x += velocity[0]
+                self.rect.y += velocity[1]
+                self.dire = velocity
+            else:
+                self.rect.x += self.dire[0]
+                self.rect.y += self.dire[1]
+            self.counter += 1
         else:
-            self.rect.x += Ghost.asustado(box, (self.rect.x//32, self.rect.y//32), pospac)[0]
-            self.rect.y += Ghost.asustado(box, (self.rect.x//32, self.rect.y//32), pospac)[1]
-
+            if self.rect.x%32 == 0 and self.rect.y%32 == 0:
+                velocity = Ghost.asustado(self.dire, box, ((self.rect.x)//32, (self.rect.y)//32), pospac)
+                self.rect.x += velocity[0]
+                self.rect.y += velocity[1]
+                self.dire = velocity
+            else:
+                self.rect.x += self.dire[0]
+                self.rect.y += self.dire[1]
+            self.counter += 1
 
 
     def distance (point1, point2):
         #calcula la distancia de un punto arbitrario a otro
-        return math.sqrt((point2[0]-point1[0])**2 + (point2[1]-point1[1])**2) 
+        return math.sqrt((point2[0]-point1[0])**2 + (point2[1]-point1[1])**2)
+
+
+    def casillaFinal (pospac, dire):
+        x = pospac[0]
+        y = pospac[1]
+        #Para rosa: calcula la casilla a la que tiene que ir (final del camino o intersección en dirección a la que va pacman)
+        while (x + dire[0], y + dire[1]) in [(2,2),(2,4),(2,6),(2,8),(2,10),(4,4),(4,6),(4,8),(6,2),(6,4),(6,6),(6,10),
+        (8,8),(8,10),(10,8),(10,10),(12,2),(12,4),(12,6),(12,10),(14,4),(14,6),(14,8),(16,2),
+        (16,4),(16,6),(16,8),(16,10)]:
+            x += dire[0]
+            y += dire[1]
+        return (pospac[0] + dire[0], pospac[1] + dire[1])
 
 
     def sinSalida (box, pos, dire, pospac):
-   		#Crea un camino imaginario en la dirección hacia la que se elige y calcula si el camino está cerrado 
+        #Crea un camino imaginario en la dirección hacia la que se elige y calcula si el camino está cerrado 
         # o si hay salidas
         if ((pos in box) and ((pos[0]+dire[1], pos[1]+dire[0]) not in box) and 
             ((pos[0]-dire[1], pos[1]-dire[0]) not in box) and ((pos[0]+dire[0], pos[1]+dire[1]) == pospac)):
@@ -69,103 +115,26 @@ class Ghost(pygame.sprite.Sprite):
             return False
 
 
-    def rojo (box, pos, pospac):
+    def alAzar (dire, box, pos, pospac):
         velocity = (0,0)
-        dic = {
-            (1,0): Ghost.distance((pos[0]+1, pos[1]), pospac), 
-            (-1,0): Ghost.distance((pos[0]-1, pos[1]), pospac), 
-            (0,1): Ghost.distance((pos[0], pos[1]+1), pospac),
-    	    (0,-1): Ghost.distance((pos[0], pos[1]-1), pospac)}
-        lista = list(dic.values())
-        menor = False
-        count = 0
-    	#Hasta que la casilla elegida sea del camino y no lleve a sinSalida busca la casilla que esté más cerca de pacman
         while True:
-            for i in range(0,4):
-                for k in range(-3+count,0):
-                    if len(lista) == 1:
-                        break
-                    if lista[i-count] >= lista[i+k-count]:
-                        menor = True
-                        count += 1
-                        break
-                    if menor == True:
-                        lista.remove(lista[i-count+1])
-                        menor = False
-            if (pos[0]+velocity[0], pos[1]+velocity[1]) in box and Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False:
-                break
+            while (velocity == (0,0) or velocity == (1,1) or velocity == (-1, -1) or
+                velocity == (1,-1) or velocity == (-1,1)):
+                velocity = (random.choice([-1,0,1]), random.choice([-1,0,1]))
+            if (Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False) and (velocity != (-dire[0], -dire[1])):
+               break
+            velocity = (0,0)
         return velocity
-    
-    
-    def naranja (box, pos, pospac):
-        velocity = (0,0)
-        #Si la distancia a pacman es menor que 3 casillas sigue como el rojo
-        if Ghost.distance(pos, pospac) <= 3:
-            return Ghost.rojo (box, pos, pospac)
-        #Si no, hasta que la casilla elegida sea del camino y no lleve a sinSalida, elige una dirección al azar
-        else:
-            while True:
-                while (velocity == (0,0) or velocity == (1,1) or velocity == (-1, -1) or
-        		    velocity == (1,-1) or velocity == (-1,1)):
-                    velocity = (random.choice([-1,0,1]), random.choice([-1,0,1]))
-                if ((pos[0]+velocity[0], pos[1]+velocity[1]) in box and 
-                    Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False):
-                    break
-            return velocity
 
 
-    def rosa (box, pos, df, pospac, esquina):
-        #Si pacman está en una de las casillas que hace esquina o intersección entre caminos
-        if (pospac in [(2,2),(2,4),(2,6),(2,8),(2,10),(4,4),(4,6),(4,8),(6,2),(6,4),(6,6),(6,10),
-        (8,8),(8,10),(10,8),(10,10),(12,2),(12,4),(12,6),(12,10),(14,4),(14,6),(14,8),(16,2),
-        (16,4),(16,6),(16,8),(16,10)]):
-            #El diccionario contiene las veces que ha ido en cada dirección al pasar por la casilla en la que está pacman
-            dic = {
-                (1,0):df.at[pospac, (1,0)], 
-                (-1,0):df.at[pospac, (-1,0)], 
-                (0,1):df.at[pospac, (0,1)], 
-                (0,-1):df.at[pospac, (0,-1)]}
-            if list(dic.values()) == [0,0,0,0]:
-                velocity = (0,0)
-                while True:
-                    while (velocity == (0,0) or velocity == (1,1) or velocity == (-1, -1) or
-                        velocity == (1,-1) or velocity == (-1,1)):
-                        velocity = (random.choice([-1,0,1]), random.choice([-1,0,1]))
-                    if ((pos[0]+velocity[0], pos[1]+velocity[1]) in box and 
-                        Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False):
-                        break
-                return Ghost.rojo(box, pos, Ghost.casillaFinal(box, pospac, velocity))       
-            lista = list(dic.values())
-            menor = False
-            count = 0
-            #Elimina los valores menores de la lista de veces que ha tomado cada dirección y si hay repetidos en el mayor valor solo lo toma una vez
-            for i in range(0,4):
-                for k in range(-3+count,0):
-                    if len(lista) == 1:
-                        break
-                    if lista[i-count] <= lista[i+k-count]:
-                        menor = True
-                        count += 1
-                        break
-                if menor == True:
-                    lista.remove(lista[i-count+1])
-                menor = False
-            return Ghost.rojo(box, pos, Ghost.casillaFinal(box, pospac, list(dic.keys())[list(dic.values()).index(lista[0])]))
-        else:
-            return Ghost.rojo(box, pos, esquina)
+    def rojo (counter, dire, box, pos, pospac):
+        if pos == (9,6) or pos == (9,5):
+            return (0,-1)
+        elif pos == (9,4):
+            return (random.choice([-1,1]), 0)
 
-
-    def azul (box, pos, df, pospac, esquina):
-        num = random.choice([0,1,2])
-        if num == 0:
-            return Ghost.rojo(box, pos, pospac)
-        elif num == 1:
-            return Ghost.naranja(box, pos, pospac)
-        else:
-            return Ghost.rosa(box, pos, df, pospac, esquina)
-
-
-    def asustado (box, pos, pospac):
+        if counter <= 720:
+            return Ghost.alAzar(dire, box, pos, pospac)
         velocity = (0,0)
         dic = {
             (1,0): Ghost.distance((pos[0]+1, pos[1]), pospac), 
@@ -177,8 +146,133 @@ class Ghost(pygame.sprite.Sprite):
         count = 0
         #Hasta que la casilla elegida sea del camino y no lleve a sinSalida busca la casilla que esté más cerca de pacman
         while True:
-            for i in range(0,4):
-                for k in range(-3+count,0):
+            for i in range(0,len(dic)):
+                for k in range(-(len(dic)-1)+count,0):
+                    if len(lista) == 1:
+                        break
+                    if lista[i-count] >= lista[i+k-count]:
+                        menor = True
+                        count += 1
+                        break
+                if menor == True:
+                    lista.remove(lista[i-count+1])
+                menor = False
+            velocity = list(dic.keys())[list(dic.values()).index(lista[0])]
+            dic.pop(velocity)
+            lista = list(dic.values())
+            count = 0
+            if ((pos[0]+velocity[0], pos[1]+velocity[1]) in box and 
+                Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False and velocity != (-dire[0], -dire[1])):
+                break
+        return velocity 
+
+    
+    def naranja (counter, dire, box, pos, pospac):
+        velocity = (0,0)
+        if pos == (10,6):
+            return (-1,0)
+        elif pos == (9,4):
+            return (random.choice([-1,1]), 0)
+        elif pos == (9,6) or pos == (9,5):
+            return (0,-1)
+
+        if counter <= 720:
+            return Ghost.alAzar(dire, box, pos, pospac)
+        #Si la distancia a pacman es menor que 3 casillas sigue como el rojo
+        if Ghost.distance(pos, pospac) <= 3:
+            return Ghost.rojo (counter, dire, box, pos, pospac)
+        #Si no, hasta que la casilla elegida sea del camino y no lleve a sinSalida, elige una dirección al azar
+        else:
+            return Ghost.alAzar (dire, box, pos, pospac)
+
+
+    def rosa (counter, dire, box, pos, df, pospac, esquina):
+        #Si pacman está en una de las casillas que hace esquina o intersección entre caminos
+        if pos == (9,6) or pos == (9,5):
+            return (0,-1)
+        elif pos == (9,4):
+            return (random.choice([-1,1]), 0)
+
+        if counter <= 720:
+            return Ghost.alAzar(dire, box, pos, pospac)
+        if (pospac in [(2,2),(2,4),(2,6),(2,8),(2,10),(4,4),(4,6),(4,8),(6,2),(6,4),(6,6),(6,10),
+        (8,8),(8,10),(10,8),(10,10),(12,2),(12,4),(12,6),(12,10),(14,4),(14,6),(14,8),(16,2),
+        (16,4),(16,6),(16,8),(16,10)]):
+            velocity = (0,0)
+            #El diccionario contiene las veces que ha ido en cada dirección al pasar por la casilla en la que está pacman
+            dic = {
+                (1,0):df.at[pospac, (1,0)], 
+                (-1,0):df.at[pospac, (-1,0)], 
+                (0,1):df.at[pospac, (0,1)], 
+                (0,-1):df.at[pospac, (0,-1)]}
+            if list(dic.values()) == [0,0,0,0]:
+                while True:
+                    while (velocity == (0,0) or velocity == (1,1) or velocity == (-1, -1) or
+                        velocity == (1,-1) or velocity == (-1,1)):
+                        velocity = (random.choice([-1,0,1]), random.choice([-1,0,1]))
+                    if ((pos[0]+velocity[0], pos[1]+velocity[1]) in box and 
+                        Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False):
+                        break
+                    velocity = (0,0)
+                if pos == Ghost.casillaFinal(pospac, velocity):
+                    return (0,0)
+                return Ghost.rojo(counter, dire, box, pos, Ghost.casillaFinal(pospac, velocity))
+            else:
+                print("dkas") 
+                lista = list(dic.values())
+                menor = False
+                count = 0
+                #Elimina los valores menores de la lista de veces que ha tomado cada dirección y si hay repetidos en el mayor valor solo lo toma una vez
+                for i in range(0,4):
+                    for k in range(-3+count,0):
+                        if len(lista) == 1:
+                            break
+                        if lista[i-count] <= lista[i+k-count]:
+                            menor = True
+                            count += 1
+                            break
+                    if menor == True:
+                        lista.remove(lista[i-count+1])
+                    menor = False
+                if pos == Ghost.casillaFinal(pospac, list(dic.keys())[list(dic.values()).index(lista[0])]):
+                    return (0,0)
+                return Ghost.rojo(counter, dire, box, pos, Ghost.casillaFinal(pospac, list(dic.keys())[list(dic.values()).index(lista[0])]))
+        else:
+            return Ghost.rojo(counter, dire, box, pos, esquina)
+
+
+    def azul (counter, dire, box, pos, df, pospac, esquina):
+        num = random.choice([0,1,2])
+        if pos == (8,6):
+            return (1,0)
+        elif pos == (9,4):
+            return (random.choice([-1,1]), 0)
+        elif pos == (9,6) or pos == (9,5):
+            return (0,-1)
+        if counter <= 720:
+            return Ghost.alAzar(dire, box, pos, pospac)
+        if num == 0:
+            return Ghost.rojo(counter, dire, box, pos, pospac)
+        elif num == 1:
+            return Ghost.naranja(counter, dire, box, pos, pospac)
+        else:
+            return Ghost.rosa(counter, dire, box, pos, df, pospac, esquina)
+
+
+    def asustado (dire, box, pos, pospac):
+        velocity = (0,0)
+        dic = {
+            (1,0): Ghost.distance((pos[0]+1, pos[1]), pospac), 
+            (-1,0): Ghost.distance((pos[0]-1, pos[1]), pospac), 
+            (0,1): Ghost.distance((pos[0], pos[1]+1), pospac),
+            (0,-1): Ghost.distance((pos[0], pos[1]-1), pospac)}
+        lista = list(dic.values())
+        menor = False
+        count = 0
+        #Hasta que la casilla elegida sea del camino y no lleve a sinSalida busca la casilla que esté más cerca de pacman
+        while True:
+            for i in range(0,len(dic)):
+                for k in range(-(len(dic)-1)+count,0):
                     if len(lista) == 1:
                         break
                     if lista[i-count] <= lista[i+k-count]:
@@ -188,22 +282,14 @@ class Ghost(pygame.sprite.Sprite):
                 if menor == True:
                     lista.remove(lista[i-count+1])
                 menor = False
+            velocity = list(dic.keys())[list(dic.values()).index(lista[0])]
+            dic.pop(velocity)
+            lista = list(dic.values())
+            count = 0
             if ((pos[0]+velocity[0], pos[1]+velocity[1]) in box and 
-                Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False):
+                Ghost.sinSalida(box, (pos[0]+velocity[0], pos[1]+velocity[1]), velocity, pospac) == False and velocity != (-dire[0], -dire[1])):
                 break
         return velocity 
-
-
-    def casillaFinal (box, pospac, dire):
-        #Para rosa: calcula la casilla a la que tiene que ir (final del camino o intersección en dirección a la que va pacman)
-        if pospac not in box:
-            return (pospac[0] - dire[0], pospac[1] - dire[1])
-        elif (((pospac[0] + dire[0], pospac[1] + dire[1]) in box) and 
-            (((pospac[0] + dire[1], pospac[1] + dire[0]) in box) or 
-             ((pospac[0] - dire[1], pospac[1] - dire[0]) in box))):
-            return (pospac[0] + dire[0], pospac[1] + dire[1])
-        else:
-            return Ghost.casillaFinal(box, (pospac[0] + dire[0], pospac[1] + dire[1]), dire)
             
             
     def set_scared_time(self, number1):
